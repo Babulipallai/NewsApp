ccb753e45b3cf9d48865fb1a361e720d2b74b5b0
Babuli Pallai <babulipalai6@gmail.com>
2023-02-20 13:01:37 +0530

    #

--- src/App.js
+++ src/App.js
@@ -1,18 +1,108 @@
 // import logo from './logo.svg';
-import './App.css';
-
-import React, { Component } from 'react'
-import Navbar from './Components/Navbar/Navbar';
-import News from './Components/News/News';
+import "./App.css";
+import React, { Component } from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import News from "./Components/News/News";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default class App extends Component {
-  
   render() {
     return (
       <div>
-        <Navbar></Navbar>
-        <News></News>
+        <Router>
+          <Navbar></Navbar>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <News
+                  key="home"
+                  pageSize={11}
+                  country="in"
+                  category="general"
+                ></News>
+              }
+            ></Route>
+            <Route
+              path="/business"
+              element={
+                <News
+                  key="business"
+                  pageSize={11}
+                  country="in"
+                  category="business"
+                ></News>
+              }
+            ></Route>
+            <Route
+              path="/entertainment"
+              element={
+                <News
+                  key="entertainment"
+                  pageSize={11}
+                  country="in"
+                  category="entertainment"
+                ></News>
+              }
+            ></Route>
+            <Route
+              path="/general"
+              element={
+                <News
+                  key="general"
+                  pageSize={11}
+                  country="in"
+                  category="general"
+                ></News>
+              }
+            ></Route>
+            <Route
+              path="/health"
+              element={
+                <News
+                  key="health"
+                  pageSize={11}
+                  country="in"
+                  category="health"
+                ></News>
+              }
+            ></Route>
+            <Route
+              path="/science"
+              element={
+                <News
+                  key="science"
+                  pageSize={11}
+                  country="in"
+                  category="science"
+                ></News>
+              }
+            ></Route>
+            <Route
+              path="/sports"
+              element={
+                <News
+                  key="sports"
+                  pageSize={11}
+                  country="in"
+                  category="sports"
+                ></News>
+              }
+            ></Route>
+            <Route
+              path="/technology"
+              element={
+                <News
+                  key="technology"
+                  pageSize={11}
+                  country="in"
+                  category="technology"
+                ></News>
+              }
+            ></Route>
+          </Routes>
+        </Router>
       </div>
-    )
+    );
   }
 }
--- src/Components/News/News.js
+++ src/Components/News/News.js
@@ -1,37 +1,118 @@
 import React, { Component } from "react";
 import NewsItem from "../NewsItem/NewsItem";
+import Spinner from "../Spinner/Spinner";
+import PropTypes from "prop-types";
 
 export default class News extends Component {
-  constructor(){
-    super();
+  static defaultProps = {
+    country: "in",
+    pageSize: 11,
+  };
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+  };
+
+  capitalizeFirstLetter = (string)=> {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+  constructor(props) {
+    super(props);
     // console.log("I am a constructor from news component");
     this.state = {
-      articles : []
-    }
+      articles: [],
+      page: 1,
+      loading: false,
+    };
+    document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey`;
   }
 
-  async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=bbeac59654f04fdaa4f9ea1f4a20cc94";
+  async updateNews() {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=bbeac59654f04fdaa4f9ea1f4a20cc94&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({articles: parsedData.articles})
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false,
+    });
   }
+
+  async componentDidMount() {
+    this.updateNews();
+  }
+
+  handlePrevClick = async () => {
+    this.setState({
+      page: this.state.page - 1,
+    });
+    this.updateNews();
+  };
+
+  handleNextClick = async () => {
+    this.setState({
+      page: this.state.page + 1,
+    });
+    this.updateNews();
+  };
+
   render() {
     return (
       <div className="container my-3">
-        <h1>NewsMonkey - Top Headlines</h1>
-        <div className="row my-3">
-          {this.state.articles.map((element)=>{
-            return <div className="col-md-4" key={element.url}>
-            <NewsItem
-              title={element.title.slice(0, 71)}
-              description={element.description.slice(0, 95)}
-              imageUrl={element.urlToImage}
-              newsUrl={element.url}
-            ></NewsItem>
-          </div>
-          })}
+        <h1 className="text-center" style={{ margin: "25px 0px" }}>
+          NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines
+        </h1>
+        {this.state.loading && <Spinner />}
+        <div className="row">
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title.slice(0, 71) : ""}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 95)
+                        : ""
+                    }
+                    imageUrl={
+                      element.urlToImage
+                        ? element.urlToImage
+                        : "https://images.indianexpress.com/2023/02/George-Soros.jpg"
+                    }
+                    newsUrl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source.name}
+                  ></NewsItem>
+                </div>
+              );
+            })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={this.handlePrevClick}
+            disabled={this.state.page <= 1}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={this.handleNextClick}
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
